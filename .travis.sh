11134075ea6af7e6c5642455134c70bd45733088
Florian Stinglmayr <florian@n0la.org>
2017-01-02 06:29:23 +0100

    Add a few more comments

--- .travis.sh
+++ .travis.sh
@@ -3,14 +3,20 @@
 set -e
 set -x
 
+# We only build master branch.
 SOURCE_BRANCH="master"
 
+# Although .travis.yml sould have a branch.except clause
+# make double sure this is not a recursive loop.
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ||
    [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy"
     exit 0
 fi
 
+# Use travis to specify an environment variable called
+# GH_TOKEN that contains a github token with which we can
+# push to the repository.
 if [ -z "${GH_TOKEN}" ]; then
     echo "No deploy token set"
     exit 1
@@ -23,5 +29,4 @@ git remote add gh-token "https://${GH_TOKEN}@github.com/ehrpg/rules.git"
 git fetch gh-token
 git fetch gh-token gh-pages:gh-pages
 
-echo "Starting build"
 mkdocs gh-deploy -v --clean -m "Automatic travis build" --remote-name gh-token
